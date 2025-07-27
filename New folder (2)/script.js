97535256fcf8eaa709e7b839f97601e2ccee0b99
maryum-hue <mabdullahmabdullah206@gmail.com>
2025-07-27 01:04:05 -0700

    Add files via upload

---
+++ New folder (2)/script.js
@@ -0,0 +1,8 @@
+let userInput = getElementById('userInput');
+let chatbotMsg = getElementById('chatBody');
+
+function userMsg() {
+    let question = userInput.value;
+    if (question.trim() === "") return;
+}
+    
